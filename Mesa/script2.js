cfcd7bcaf859454be7cac0a15aee57704e34bc53
Goncalo Marques <goncalo.cool@gmail.com>
2017-04-14 18:53:29 +0100

    Numeros de pagamento mudam

--- Mesa/script2.js
+++ Mesa/script2.js
@@ -1,3 +1,4 @@
+
 //Clicar nos botoes das quantidades
 function CustomOnClick(index, add) {
 	var nome = "Quantidade" + index;
@@ -10,4 +11,19 @@ function CustomOnClick(index, add) {
 		old--;
 	}
 	document.getElementById(nome).innerHTML = old;
+	UpdatePriceText();
+}
+
+function UpdatePriceText() {
+
+	var PlateNum = 2;
+	var contador = 0;
+	var i;
+	for (i = 0; i < PlateNum; i++) {
+		var Texto1 = "Quantidade" + (i+1);
+		var Texto2 = "TextoPrato" + (i+1);
+		contador += parseFloat(parseInt(document.getElementById(Texto1).innerHTML) * parseFloat(document.getElementById(Texto2).innerHTML));
+	}
+
+	document.getElementById("TotalTexto").innerHTML = contador;
 }
