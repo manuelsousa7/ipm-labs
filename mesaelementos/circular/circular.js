aba2839f569f401aced0d128a05e99550b0e9303
Manuel Sousa <mvs@macbook.local>
2017-04-19 11:15:43 +0100

    new stuff

--- mesaelementos/circular/circular.js
+++ mesaelementos/circular/circular.js
@@ -1,7 +1,7 @@
 toggle = document.querySelectorAll(".toggle")[0];
 nav = document.querySelectorAll("nav")[0];
-toggle_open_text = 'Menu';
-toggle_close_text = 'Close';
+toggle_open_text = '';
+toggle_close_text = '';
 
 toggle.addEventListener('click', function() {
 	nav.classList.toggle('open');
